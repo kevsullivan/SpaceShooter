74a61b3d7596ffa25054248346b8bba7a1a4ff87
kevsullivan <kevsullivan@live.ie>
2017-09-26 00:24:31 +0100

    Update game controller wave spawning logic

--- Assets/Scripts/GameScripts/GameController.cs
+++ Assets/Scripts/GameScripts/GameController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+// Struct which will provide config in insepctor for spawn logic
+// spawnObject: The object to spawn
+// spawnProbability: The probability of spawning the spawnObject
+//                   Note: Probabilities are 0.0 -> 1.0 and must add to 1.0
+[System.Serializable]
+public struct SpawnProbability
+{
+    public GameObject spawnObject;
+    public float spawnProbability;
+}
 
-    public GameObject[] hazards;
+public class GameController : MonoBehaviour {
+    
+    // Wave spawning attributes: Ispector configurable
+    public List<SpawnProbability> hazardsAndProbability;
     public Vector3 spawnValues;
     public int hazardCount;
+    // Wave spawning attributes: Game controller logic
+    private bool spawning;
+    private float probability;
+    private float cumlative_probability;
 
     // Spawnwait == How long to wait before looping to next spawn of Hazard
     public float spawnWait;
@@ -64,16 +80,32 @@ public class GameController : MonoBehaviour {
             for (int i = 0; i < hazardCount; i++)
             {
                 // Get random hazard from list of hazards
-                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
-                // Calculate a random spawn position
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                // Roll a dice to get random probability (0.0 inclusive to 1.0 inclusive)
+                probability = Random.value;
+                cumlative_probability = 0.0f;
+                spawning = true;
+                
+                // Got through hazards and if probability works out, spawn the selected GameObject
+                for (int j = 0; j < hazardsAndProbability.Count && spawning; j++)
+                {
+                    cumlative_probability += hazardsAndProbability[j].spawnProbability;
+                    if (probability < cumlative_probability)
+                    {
+                        GameObject spawn = hazardsAndProbability[j].spawnObject;
 
-                // Instantiate the hazard object at the random spawn position we generated
-                // The quaternion identity corresponds to "no rotation" - the object is perfectly aligned with the world or parent axes
-                // I.e. our Asteroid (hazard) prefabs are already rotated as intended.
-                Instantiate(hazard, spawnPosition, Quaternion.identity);
+                        // Calculate a random spawn position
+                        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                        // Instantiate the hazard object at the random spawn position we generated
+                        // The quaternion identity corresponds to "no rotation" - the object is perfectly aligned with the world or parent axes
+                        // I.e. our Asteroid (hazard) prefabs are already rotated as intended.
+                        Instantiate(spawn, spawnPosition, Quaternion.identity);
+                        spawning = false;
+                    }
+                }
+                // Wait for next spawn
                 yield return new WaitForSeconds(spawnWait);
             }
+            // Wait a bit before starting a new wave
             yield return new WaitForSeconds(waveWait);
             if (GameManager.instance.gameOver)
             {
